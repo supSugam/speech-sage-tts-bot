d53c8ffcad3c617a3cd76715c04e934f53a7ca73
supSugam <sugamsubedi234@gmail.com>
2024-06-06 19:47:37 +0545

    chore: Update .gitignore to exclude dist/ directory and remove src/store/

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A Telegram bot that converts text to voice of your choice.
 
+### Screenshots & More Details at: [MyPortfolioWebsite/Projects/SpeechSageTTSBot](https://sugamsubedi.com.np/projects/speech-sage-tts-bot) 🚧
+
 ## Features
 
 - Choose from a variety of voices.
@@ -14,31 +16,31 @@ To get started with the Speech Sage TTS Bot, follow these steps:
 
 1. **Clone the repository:**
 
-    ```bash
-    git clone https://github.com/your-username/speech-sage-tts-bot.git
-    ```
+   ```bash
+   git clone https://github.com/supSugam/speech-sage-tts-bot.git
+   ```
 
 2. **Install dependencies:**
 
-    ```bash
-    cd speech-sage-tts-bot
-    npm install
-    ```
+   ```bash
+   cd speech-sage-tts-bot
+   npm install
+   ```
 
 3. **Set up your Telegram bot token:**
 
-    - Create a `.env` file in the root directory.
-    - Add your Telegram bot token to the `.env` file:
+   - Create a `.env` file in the root directory.
+   - Add your Telegram bot token to the `.env` file:
 
-        ```
-        BOT_TOKEN=your-telegram-bot-token
-        ```
+     ```
+     BOT_TOKEN=your-telegram-bot-token
+     ```
 
 4. **Build and start the bot:**
 
-    ```bash
-    npm start
-    ```
+   ```bash
+   npm start
+   ```
 
 5. **Interact with your bot in Telegram.**
 
@@ -51,5 +53,3 @@ To get started with the Speech Sage TTS Bot, follow these steps:
 ## Contributing
 
 Contributions are welcome! Feel free to open issues and pull requests.
-
-
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,10 +1,57 @@
+// export function splitStringIntoChunks(
+//   inputString: string,
+//   chunkSize: number
+// ): string[] {
+//   const result: string[] = [];
+//   const sentences = inputString.match(/[^.!?]+[.!?]+/g); // Extracting sentences
+//   if (!sentences) return result; // Return empty array if no sentences found
+
+//   let currentChunk = '';
+//   for (const sentence of sentences) {
+//     if (currentChunk.length + sentence.length <= chunkSize) {
+//       currentChunk += sentence;
+//     } else {
+//       result.push(currentChunk);
+//       currentChunk = sentence;
+//     }
+//   }
+
+//   if (currentChunk) {
+//     result.push(currentChunk); // Push any remaining chunk
+//   }
+
+//   return result;
+// }
+
 export function splitStringIntoChunks(
   inputString: string,
-  chunkSize: number
+  chunkSize: number,
+  singleSentenceMode = false
 ): string[] {
-  const result: string[] = [];
-  for (let i = 0; i < inputString.length; i += chunkSize) {
-    result.push(inputString.slice(i, i + chunkSize));
+  let chunks: string[] = [];
+  const sentences = inputString.split(/(?<=[.?!;\s])\s/g).map((s) => {
+    const endsWith = s.charAt(s.length - 1);
+    if (endsWith.match(/[.?!;]/)) {
+      const regex = new RegExp(`\\${endsWith}+`, 'g');
+      const replacedString = s.replace(regex, `${endsWith} `);
+      return replacedString;
+    }
+    return s;
+  });
+
+  if (singleSentenceMode) return sentences;
+
+  let str = '';
+  for (const sentence of sentences) {
+    if (chunkSize - str.length >= sentence.length) {
+      str += sentence;
+    } else {
+      chunks.push(chunks.length === 0 ? str : ` ${str.trim()}`);
+      str = sentence;
+    }
   }
-  return result;
+  if (str) chunks.push(chunks.length === 0 ? str : ` ${str.trim()}`);
+
+  console.log(chunks);
+  return chunks;
 }
